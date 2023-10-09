ffd06051b2b45c7a7032c4d0b526bbfb83d9723a
Ashwin <83401545+ashwinl12@users.noreply.github.com>
2023-10-09 22:17:40 +0530

    Added weather widget Code

---
+++ weather.js
@@ -0,0 +1,4 @@
+<a class="weatherwidget-io" href="https://forecast7.com/en/28d9877d71/meerut/" data-label_1="MEERUT" data-label_2="Uttar Pradesh, India" data-font="Ubuntu" data-icons="Climacons Animated" data-mode="Forecast" data-days="3" data-theme="sky" >MEERUT Uttar Pradesh, India</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
